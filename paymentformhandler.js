3cfed0ad8dcbc24bd3af647a9399e79c8fc18398
RyanJJ7 <71418585+RyanJJ7@users.noreply.github.com>
2021-10-01 23:29:44 -0700

    Update paymentformhandler.js

--- paymentformhandler.js
+++ paymentformhandler.js
@@ -1,11 +1,41 @@
 (function (window) {
     'use strict';
-    var FORM_SELECTOR = '[data-payment-info="form"]';
-    var FormHandler = App.FormHandler;
-    var formHandler = new FormHandler(FORM_SELECTOR);
+    var App = window.App || {};
+    var $ = window.jQuery;
 
-    formHandler.addSubmitHandler(function(title, name) {
-        console.log('Thank you for your payment, ' + title + ' ' + name);
-    });
-    console.log(formHandler);
+    class FormHandler {
+        constructor(selector) {
+            console.log('In FormHandler constructor...');
+        
+            if (!selector) { throw new Error('No selector provided'); }
+        
+            this.$formElement = $(selector);
+            if (this.$formElement.length === 0) {
+                throw new Error('Could not find element with selector: ' + selector);
+            }
+        }
+    
+        addSubmitHandler() {
+            var modal = document.getElementById("modal");
+            var btn = document.getElementById("submit");
+            var span = document.getElementsByClassName("close")[0];
+            
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }
+    }
+    
+    App.FormHandler = FormHandler;
+    window.App = App;
 })(window);
